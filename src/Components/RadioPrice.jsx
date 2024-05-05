f749f60db2da15e5067c52cd59b92fc8cacf033d
Diljot Singh <singhdiljot2306@gmail.com>
2024-05-05 14:49:23 +0530

    added add tokens radio inputs

---
+++ src/Components/RadioPrice.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const RadioPrice = ({ radioPriceButtons, price, setPrice }) => {
+    return (
+        <label htmlFor={`${radioPriceButtons.name}-${radioPriceButtons.price}`} className="block cursor-pointer border border-gray-500 rounded-lg">
+            <input
+                type="radio"
+                id={`${radioPriceButtons.name}-${radioPriceButtons.price}`}
+                name={radioPriceButtons.name}
+                className="hidden"
+                value={radioPriceButtons.name}
+                onChange={() => setPrice(radioPriceButtons)}
+                checked={price.name === radioPriceButtons.name} // Ensure the correct radio button is checked
+            />
+            <div className={`p-4 rounded-lg border-2 ${price.name === radioPriceButtons.name && "border-blue-500 bg-blue-100"} cursor-pointer transition-colors hover:border-blue-500 hover:bg-blue-100`}>
+                <div className="flex justify-between items-center">
+                    <div className="flex">
+                        {/* <div className="h-6 w-6 rounded-full border-2 border-gray-400 mr-2"></div> */}
+                        <div className={`h-6 w-6 rounded-full ${price.name === radioPriceButtons.name ? "border-blue-500 bg-blue-400" : "border-2 border-gray-400"}  mr-2`}></div>
+                        <div className="font-semibold">{radioPriceButtons.name}</div>
+                    </div>
+                    <span className="text-gray-600">₹{radioPriceButtons.price}</span>
+                </div>
+            </div>
+        </label>
+    );
+};
+
+export default RadioPrice;
--- src/Pages/AddBalance.jsx
+++ src/Pages/AddBalance.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FaCopy, FaRegCopy } from "react-icons/fa";
 import { Footer } from '../Components/Footer';
+import RadioPrice from '../Components/RadioPrice';
+import { Button } from "flowbite-react";
+
 
 const AddBalance = () => {
     const curBalance = useSelector(state => state.balance);
@@ -9,56 +12,94 @@ const AddBalance = () => {
 
     const [copied, setCopied] = useState(false);
     const [value, setvalue] = useState("http://localhost:5173" + "/refer/" + curUser.id)
+    const [price, setPrice] = useState({ name: null, price: null });
+    const [radioPriceButtons, setradioPriceButtons] = useState([
+        {
+            name: "10 Token",
+            price: 10,
+        },
+        {
+            name: "20 Token",
+            price: 20,
+        },
+        {
+            name: "50 Token",
+            price: 50,
+        },
+        {
+            name: "100 Token",
+            price: 100,
+        }
+    ])
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(value);
         setCopied(true);
-        setTimeout(() => setCopied(false), 2000); 
+        setTimeout(() => setCopied(false), 2000);
     };
+
+    const handleBuyNow = () => {
+        console.log(price)
+    }
 
     return (
         <>
-        <div className="mt-[5rem] flex justify-center flex-col items-center h-[60vh]">
-            <div className="text-center">
-                <div className="text-xl">Your current balance is:</div>
-            </div>
-            <div className="border border-black rounded-md w-48 h-24 flex justify-center items-center mt-4">
-                <pre className="font-bold text-xl">Tokens: </pre>
-                <div className="text-xl">{curBalance.amount}</div>
-            </div>
-            <div className="my-4 border border-gray w-[80%]"></div>
-            <div>
-                <div className="text-xl mb-2">Refer to earn 10 Tokens</div>
-            </div>
-            {/* copyt to clipboard functionality */}
-            <div className="w-full max-w-[35rem]">
-                <div className="relative mx-7">
-                    <input
-                        type="text"
-                        className="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        value={value}
-                        disabled
-                        readOnly
-                    />
-                    <button
-                        onClick={copyToClipboard}
-                        className="absolute end-2.5 top-1/2 -translate-y-1/2 text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border"
-                    >
-                        <span className={copied ? "hidden" : "inline-flex items-center"}>
-                            <FaCopy />
-                            <span className="ml-1 text-xs font-semibold">Copy</span>
-                        </span>
-                        <span className={copied ? 'inline-flex items-center' : 'hidden'}>
-                            <FaRegCopy />
-                            <span className="ml-1 text-xs font-semibold text-blue-700 dark:text-blue-500">
-                                Copied
+            <div className="mt-[5rem] flex justify-center flex-col items-center">
+                <div className="text-center">
+                    <div className="text-xl">Your current balance is:</div>
+                </div>
+                <div className="border border-black rounded-md w-48 h-24 flex justify-center items-center mt-4">
+                    <pre className="font-bold text-xl">Tokens: </pre>
+                    <div className="text-xl">{curBalance.amount}</div>
+                </div>
+                <div className="my-4 border border-gray w-[80%]"></div>
+                {/* price radio box */}
+                <div>
+                    <div className="p-6 w-[35rem]">
+                        <div>
+                            <div className="text-xl mb-2 text-center">Select your price</div>
+                        </div>
+                        <div className="flex flex-col gap-4">
+                            {radioPriceButtons.map(button =>
+                                <RadioPrice key={button.name} radioPriceButtons={button} price={price} setPrice={setPrice} />
+                            )}
+                        </div>
+                        <Button onClick={handleBuyNow} className="bg-blue-600 mt-4 enabled:hover:bg-blue-700 w-[100%]" type="button" disabled={!price.name}>Buy Now</Button>
+                    </div>
+                </div>
+                <div className="my-4 border border-gray w-[80%]"></div>
+                {/* copy to clipboard functionality */}
+                <div>
+                    <div className="text-xl mb-2">Refer to earn 10 Tokens</div>
+                </div>
+                <div className="w-full max-w-[35rem]">
+                    <div className="relative mx-7">
+                        <input
+                            type="text"
+                            className="col-span-6 bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            value={value}
+                            disabled
+                            readOnly
+                        />
+                        <button
+                            onClick={copyToClipboard}
+                            className="absolute end-2.5 top-1/2 -translate-y-1/2 text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border"
+                        >
+                            <span className={copied ? "hidden" : "inline-flex items-center"}>
+                                <FaCopy />
+                                <span className="ml-1 text-xs font-semibold">Copy</span>
                             </span>
-                        </span>
-                    </button>
+                            <span className={copied ? 'inline-flex items-center' : 'hidden'}>
+                                <FaRegCopy />
+                                <span className="ml-1 text-xs font-semibold text-blue-700 dark:text-blue-500">
+                                    Copied
+                                </span>
+                            </span>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <Footer />
+            <Footer />
         </>
     )
 }
